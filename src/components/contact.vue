1f0f22d1acf33239b9277368bcb165e0bb4bf138
Roland HUON <huonroland91@gmail.com>
2024-01-19 12:40:32 +0100

    ✨ components creation contacts

---
+++ src/components/contact.vue
@@ -0,0 +1,47 @@
+<script>
+import { useAddContactStore } from '@/stores/addContact'
+export default{
+    name: 'CartePerso',
+    setup(){
+        const personneStore = useAddContactStore()
+        return{
+          personneStore,
+        }
+    },
+    computed:{
+        contacts(){
+            return this.personneStore.personnes
+        }
+    },
+    
+}
+</script>
+<template>
+    <div class="contact-single" v-for="contact in contacts">
+        <h2>{{ contact.nom }}</h2>
+        <p>{{ contact.number }}</p>
+        <img src="../assets/appel-icon.png" alt="appel icon"> 
+    </div>
+</template>
+<style scoped>
+    .contact-single{
+    display: flex;
+    justify-content: space-between;
+  }
+  h2{
+    text-align: left;
+    font-size: 20px;
+    font-weight: 500;
+    color: black;
+    letter-spacing: 1.04px;
+  }
+  p{
+    font-size: 20px;
+    font-weight: 500;
+    color: rgba(74, 73, 73, 0.797);
+    letter-spacing: 1.04px;
+  }
+  img{
+    width: 5%;
+  }
+</style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,17 +1,10 @@
 <script>
-import { useAddContactStore } from '@/stores/addContact'
+import contact from '../components/contact.vue'
+
   export default{
     name: 'ContactView',
-    setup(){
-        const personneStore = useAddContactStore()
-        return{
-          personneStore,
-        }
-    },
-    computed:{
-        contacts(){
-            return this.personneStore.personnes
-        }
+    components:{
+      contact,
     },
     methods:{
         adds(){
@@ -25,11 +18,7 @@ import { useAddContactStore } from '@/stores/addContact'
   <div class="contact-global-content">
     <button @click="adds()">+</button>
     <div class="contact-part">
-      <div class="contact-single" v-for="contact in contacts">
-        <h2>{{ contact.nom }}</h2>
-        <p>{{ contact.number }}</p>
-        <img src="../assets/appel-icon.png" alt="appel icon">
-      </div>
+      <contact />
     </div>
   </div>
 </template>
@@ -43,31 +32,17 @@ import { useAddContactStore } from '@/stores/addContact'
     flex-direction: column;
     gap: 40px;
   }
-  .contact-single{
+  .contact-part{
     display: flex;
-    justify-content: space-between;
+    flex-direction: column;
+    gap: 20px;
   }
+  
   button{
     width: 50px;
     height: auto;
     background-color: orange;
     align-self: end;
     font-size: 24px;
-  }
-  h2{
-    text-align: left;
-    font-size: 20px;
-    font-weight: 500;
-    color: black;
-    letter-spacing: 1.04px;
-  }
-  p{
-    font-size: 20px;
-    font-weight: 500;
-    color: rgba(74, 73, 73, 0.797);
-    letter-spacing: 1.04px;
-  }
-  img{
-    width: 5%;
   }
 </style>
